3b33a7cac92c3b1dba1d7724fce78af0407802e2
Ahmed Tareq <63518121+AhmedTareq2050@users.noreply.github.com>
2021-06-23 22:12:28 +0200

    Create cnamer.py

---
+++ cnamer.py
@@ -0,0 +1,44 @@
+import dns.resolver, ssl
+import requests
+from sys import argv
+
+domains = open(argv[1], "r").read().splitlines()
+
+for domain in domains :
+    
+    try :
+    
+        cname_domains = dns.resolver.query(domain, "CNAME")
+        
+        print(f"{domain} :")
+        
+        for cname_domain in cname_domains :
+            
+            name = cname_domain.target
+            
+            if "http://" not in name or "https://" not in name :
+            
+                name  = "http://" + str(name)
+            
+                try :
+            
+                    site = requests.get(name)
+                
+                    print(name + f"  ({site.status_code})")
+            
+                except requests.exceptions.SSLError :
+                    import urllib3
+                    
+                    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+                    
+                    site = requests.get(name, verify=False)
+                    
+                    print(name + f"  ({site.status_code})")
+            
+            
+        print("\n" + "#" * 50)
+        
+    except dns.resolver.NoAnswer :
+    
+        print(f"{domain} :")
+        print("not found\n\n" + "#" * 50 + "\n")
